fd41c3e5b78afdceb6c5fb8d1aac54babc7ab03f
ihor97 <daniel.waffner@gmail.com>
2023-06-06 20:24:04 +0100

    Дана дата в формате год-месяц-день. Преобразуйте эту дату в формат день.месяц.год.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let date=new Date()
-let hours,minutes,seconds,days,month,years
-date.getHours()<10 ? hours='0'+date.getHours() :hours=date.getHours()
-date.getMinutes()<10 ? minutes='0'+date.getMinutes() :minutes=date.getMinutes()
-date.getSeconds()<10 ? seconds='0'+date.getSeconds() :seconds=date.getSeconds()
-days=date.getDate()
-month=+date.getMonth()+1
-years=date.getFullYear()
+let str='год-месяц-день'
 
-console.log(hours+':'+minutes+':'+seconds+' '+days+'.'+month+'.'+years);
+console.log(str.split('-').reverse().join('/'));

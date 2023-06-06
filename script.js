355a8545320138cc0c071d7f04bbf25446959955
ihor97 <daniel.waffner@gmail.com>
2023-06-06 22:28:44 +0100

    Разность между датами в формате timestamp в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /*
-Существует специальный метод getTime, с
- помощью которого можно получить время в формате timestamp getTime()
-
+Формат timestamp предназначен для нахождения разницы между датами.
 */
 
+let now=new Date()
+let birth=new Date(1997,7,5)
+let diff=(now.getTime()-birth.getTime())/(1000*60*60*24*365)
+console.log(diff);

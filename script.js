8a6e93bdbaf57d3bfb2e4f9e79065d21b9485cb7
ihor97 <daniel.waffner@gmail.com>
2023-06-06 20:27:07 +0100

    Получения дня недели на JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let str='год-месяц-день'
-
-console.log(str.split('-').reverse().join('/'));
+let date=new Date()
+let hours,minutes,seconds,days,month,years
+date.getHours()<10 ? hours='0'+date.getHours() :hours=date.getHours()
+/*
+С помощью объекта Date можно также получить номер текущего дня недели.
+ Делается это с помощью метода getDay. Этот метод возвращает числа от 0 до 6-ти,
+ причем неделя начинается с воскресенья и этот день имеет номер 0
+*/ 

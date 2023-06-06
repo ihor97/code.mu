f9a6cd9d3f6fb51d4b04e0a524ed3b0e68258c3b
ihor97 <daniel.waffner@gmail.com>
2023-06-06 20:28:56 +0100

    Вывод частей даты словом в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let date=new Date()
-let hours,minutes,seconds,days,month,years
-date.getHours()<10 ? hours='0'+date.getHours() :hours=date.getHours()
-/*
-С помощью объекта Date можно также получить номер текущего дня недели.
- Делается это с помощью метода getDay. Этот метод возвращает числа от 0 до 6-ти,
- причем неделя начинается с воскресенья и этот день имеет номер 0
-*/ 
+let months = [
+	'янв', 'фев', 'мар', 'апр', 'май', 
+		'июн', 
+	'июл', 'авг', 'сен', 'окт', 'ноя', 
+		'дек' 
+];
+
+let month=(new Date()).getMonth()
+
+console.log(months[month]);

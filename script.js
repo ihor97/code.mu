500fa9da1a153449da2892fdb5e8f19c6a6e7e8f
ihor97 <daniel.waffner@gmail.com>
2023-06-07 22:08:18 +0100

    Добавление анонимных обработчиков в цикле в JavaScript

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 /*
-Для этого существует метод querySelectorAll, 
-получающий все теги, подпадающие под 
-CSS селектор, в виде массива элементов.
+
 */
 let elems = document.querySelectorAll('p');
 
 for (let elem of elems) {
-	elem.addEventListener('click', func);
-}
-function func() {
-	console.log(this.textContent); // выводим 	текст абзаца 
+	elem.addEventListener('click', function() {
+		console.log(this.textContent);
+	});
 }

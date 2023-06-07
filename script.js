6958e47297d6294ad927fbc9a7f9ba3d390a2b8b
ihor97 <daniel.waffner@gmail.com>
2023-06-07 21:58:28 +0100

    Получение группы элементов в JavaScript

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 /*
-Получится, что у нас есть три кнопки. Нажатие на каждую кнопку будет приводить к вызову функции func. 
-При этом при каждом клике this будет содержать ссылку на ту кнопку, в которой произошло событие.
-
-То есть каждое нажатие будет выводить в консоль value той кнопки, на которой произошло нажатие, 
-но делать это будет одна и та же функция func! Вот в чем преимущество использования this.
+Для этого существует метод querySelectorAll, 
+получающий все теги, подпадающие под 
+CSS селектор, в виде массива элементов.
 */
-let button1 = document.querySelector('#button1');
-let button2 = document.querySelector('#button2');
-let button3 = document.querySelector('#button3');
+let elems = document.querySelectorAll('.www');
 
-button1.addEventListener('click', func);
-button2.addEventListener('click', func);
-button3.addEventListener('click', func);
-
-function func() {
-	console.log(this.value);
+for (let elem of elems) {
+	elem.textContent = elem.textContent 
+		+ '!'; 
 }

554e54689de61f8c3a6a7d361c38d7a2232f623a
ihor97 <daniel.waffner@gmail.com>
2023-06-07 21:19:30 +0100

    Сложные селекторы DOM элемента в JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// textContent текст тега
+// складний селектор 
+/*
+Метод querySelector, однако, работает не так.
+ Он всегда получает только один элемент - первый,
+ попавший под указанный селектор.
+
+*/
+let elem = document.querySelector('#parent input');
+console.log(elem);

7f555573c7bbbd0a7f217eeab0566e7e8ffaa7ed
ihor97 <daniel.waffner@gmail.com>
2023-06-08 17:30:38 +0100

    Элемент события в объекте Event в JavaScript

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 
 /*
-Объект Event также содержит в себе тип произошедшего события.
- Под типом понимается название click, 
-mouseover и так далее. Тип события содержит свойство type:
+Дело в том, что на самом деле this всегда содержит элемент, к 
+которому было привязано событие, а свойство target - элемент, по которому реально был клик. 
+Этот реальный элемент может совпадать с this, а может не совпадать.
 
+В описанном случае получится, что свойство target будет содержать конечный тег, 
+в котором случилось событие - то есть абзац, а не див
 
-
+Свойство tagName содержит имя тега в верхнем регистре (большими буквами).
 */
-function func(e) {
-	if(e.type==='click'){
-		this.style.backgroundColor='green'
-	}
-	if(e.type==='dblclick'){
-		this.style.backgroundColor='red'
-	}
-}
+
+// let elem = document.querySelector('#elem');
+
+// elem.addEventListener('click', function(event) {
+// 	console.log(event.target); // выведет  наш абзац 
+// 	console.log(this);         // выведет наш див 
+// });
+
 let elem = document.querySelector('#elem');
 
-elem.addEventListener('click', func.bind(elem));
-elem.addEventListener('dblclick', func.bind(elem));
+elem.addEventListener('click',e=>{
+	if(e.target.tagName==='LI'){
+		e.target.textContent+='!'
+	}
+	
+	if(e.target.tagName==='UL'){
+		console.log('ul');
+	}
+
+})
 

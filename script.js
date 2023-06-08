c98e8ba86820a21c74b8ca175a7ed9bc0ae4032f
ihor97 <daniel.waffner@gmail.com>
2023-06-08 18:18:52 +0100

    Несколько обработчиков на элементе в JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
 /*
-Всплытие события можно остановить на любой элемент, через который всплывает событие. 
-Для этого в коде элемента следует вызвать метод stopPropagation объекта Event.
+Если у элемента есть несколько обработчиков на одно событие, 
+то даже при прекращении всплытия все они будут выполнены. 
+То есть, stopPropagation препятствует продвижению события дальше, 
+но на текущем элементе все обработчики отработают. Смотрите пример:
 
 elem1.addEventListener('click', function() {
 	console.log('зеленый');
 });
 elem2.addEventListener('click', function(event) {
-	console.log('голубой');
+	console.log('голубой - первый 
+		обработчик'); 
 	event.stopPropagation(); // остановим 
 		всплытие 
+});
+elem2.addEventListener('click', function() {
+	console.log('голубой - второй обработчик'); 
+		// все равно сработает 
 });
 elem3.addEventListener('click', function() {
 	console.log('красный');

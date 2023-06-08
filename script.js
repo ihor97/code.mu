fd77f34923d29e76351e155972999e95b40cad4e
ihor97 <daniel.waffner@gmail.com>
2023-06-08 18:05:36 +0100

    Отслеживание клавиш-модификаторов в JavaScript

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 
 /*
-В свойстве key объекта с событием будет лежать нажатый символ:
-elem.addEventListener('keypress', function(event) {
-	console.log(event.key);
-});
+С помощью объекта Event можно узнать, 
+были ли нажаты в момент события клавиши Ctrl, Alt и Shift.
+ Это делается с помощью свойств ctrlKey, altKey и shiftKey -
+  они имеют значение true или false в зависимости от того,
+ была ли нажата эта клавиша в момент события или нет.
 
-А в свойстве code будет лежать код нажатой клавиши:
-
-elem.addEventListener('keypress', function(event) {
-	console.log(event.code);
-});
-key -клавіша на клавіатурі
-code: 'KeyD'
-keyCode-номерний код клавіші
-
+ let elem = document.querySelector('#elem');
+ 
+ elem.addEventListener('click', function(event) {
+	 if (event.ctrlKey) {
+		 console.log('нажат Ctrl');
+	 }
+	 
+	 if (event.altKey) {
+		 console.log('нажат Alt');
+	 }
+	 
+	 if (event.shiftKey) {
+		 console.log('нажат Shift');
+	 }
+ });
 */
-let elem = document.querySelector('input');
-let div = document.querySelector('div');
 
+let elem = document.querySelector('#elem');
 
-elem.addEventListener('keypress', function (event) {
-	if (event.key === 'Enter') {
-		div.textContent =this.value
-		this.value=''
+elem.addEventListener('click',e=>{
+	if(e.target.tagName==='LI'){
+		if(e.ctrlKey){
+			e.target.textContent+='1'
+		}
+		if(e.shiftKey ){
+			e.target.textContent+='2'
+		}
 	}
-
-});
+})

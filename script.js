8df91d903a53014bc7aaca6f6ff84089316e6058
ihor97 <daniel.waffner@gmail.com>
2023-06-11 22:51:24 +0100

    Обращение к атрибутам через методы в JavaScript

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 /*
-Для обращения к таким атрибутам следует использовать camelCase:
+К пользовательским атрибутам можно также обращаться с помощью методов типа getAttribute,
+ в этом случае следует писать полное название атрибута:
 */ 
 
 let elem = document.querySelector('#elem');
-console.log(elem.dataset.myTest);
+
+console.log(elem.getAttribute('data-num'));    // выведет 1000 
+console.log(elem.getAttribute('data-my-num')); // выведет 2000

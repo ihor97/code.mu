d5c5a1a5a5e4648d733876ff1f3b1d330e900e70
ihor97 <daniel.waffner@gmail.com>
2023-06-11 10:23:37 +0100

    Получение атрибутов в JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+/*
+Метод getAttribute считывает значение заданного атрибута у тега.
+*/ 
+
+let elem = document.querySelector('#elem');
+let value = elem.getAttribute('value');
+
+console.log(value);

f4b2782eb477de8c9efc4376345a392f9f8dd0d4
ihor97 <daniel.waffner@gmail.com>
2023-06-12 18:31:02 +0100

    Применение cтилизации в JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 /*
-Свойство float является исключением, так как оно является специальным в JavaScript. 
-Для него следует писать cssFloat:
+Давайте сделаем кнопку, по нажатию на которую элемент будет то показываться, 
+то скрываться. Пусть по умолчанию элемент скрыт (это реализуем с помощью display: none), 
+а покажется он с помощью добавления класса active.
+ Этот класс будем то добавлять, то убирать с помощью classList.toggle:
 
 
 */ 
+let button = document.querySelector('#button');
 let elem = document.querySelector('#elem');
-elem.style.cssFloat = 'right';
+
+button.addEventListener('click', function() {
+	elem.classList.toggle('active');
+});

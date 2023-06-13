16cbac122dcea890283ab21cfc3b67a8855d548e
ihor97 <daniel.waffner@gmail.com>
2023-06-13 20:56:48 +0100

    Событие change в JavaScript

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 /*
-Давайте теперь поработаем с радиокнопками. 
-Они представляют собой группу переключателей, 
-из которых может быть выбран только один. 
-Чтобы несколько радиокнопочек были группой, 
-они должны иметь одинаковое значение атрибута name:
-
-Давайте сделаем так, чтобы по нажатию на кнопку на экран
- вывелось value той радиокнопочки, которая отмечена в данный момент.
+В данном уроке вы с вами разберем событие change, возникающее в полях ввода при их изменениях. 
 */ 
 
-let radios = document.querySelectorAll('input[type="radio"]');
-let button = document.querySelector('#button');
+let elem = document.querySelector('#elem');
 
-button.addEventListener('click', function() {
-	for (let radio of radios) {
-		if (radio.checked) {
-			console.log(radio.value);
-		}
-	}
+elem.addEventListener('change', function() {
+	console.log(this.value);
 });

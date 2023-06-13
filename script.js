83854eda82db4af0562d6012ba2e46e6abbc1a6b
ihor97 <daniel.waffner@gmail.com>
2023-06-13 20:57:39 +0100

    Событие input в JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /*
-В данном уроке вы с вами разберем событие change, возникающее в полях ввода при их изменениях. 
-*/ 
+Следующее событие, которое мы с вами изучим, называется input.
+ Оно возникает каждый раз при вводе нового символа в инпут или textarea.*/ 
 
-let elem = document.querySelector('#elem');
+ let elem = document.querySelector('#elem');
 
-elem.addEventListener('change', function() {
-	console.log(this.value);
-});
+ elem.addEventListener('input', function() {
+     console.log(this.value);
+ });
